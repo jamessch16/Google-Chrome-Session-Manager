c4178647078ec760e32d0da868a3e711731ca610
jamessch16 <jamessch16@gmail.com>
2021-04-24 01:34:44 -0600

    icons

--- src/sessionManager.js
+++ src/sessionManager.js
@@ -1,8 +1,14 @@
-function onPageCreate() {}
+function storeSession() {}
+
+function buttonHandler() {
+    document.getElementById("test").innerHTML = "Pressed";
+}
 
 // Session is a list of lists of strings
-function storeSession(session) {}
-
 function loadSession(session) {}
 
 function getSession() {}
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("save_button").addEventListener("click", buttonHandler);
+});
